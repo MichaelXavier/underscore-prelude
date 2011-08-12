173214b914bb5c051c9b8e0e5bb43a949c426631
Michael Xavier <michael@michaelxavier.net>
2011-08-11 18:53:38 -0700

    working my way to list operations

--- underscore-prelude.js
+++ underscore-prelude.js
@@ -1,3 +1,4 @@
+//TODO: preserve context?
 _.mixin({
   fst: function(pair) {
     return pair[0]; 
@@ -16,6 +17,14 @@ _.mixin({
   //TODO: Enum
 
   //TODO: Bounded
+  '+': function(x, y) {
+    return x + y;
+  },
+
+  '*': function(x, y) {
+    return x + y;
+  },
+  //TODO: more "operators
 
   negate: function(x) {
     return -x;
@@ -58,5 +67,113 @@ _.mixin({
 
   not: function(x) {
     return !x;
+  },
+
+  pi: function() {
+    return Math.PI;
+  },
+
+  exp: Math.exp,
+
+  sqrt: Math.sqrt,
+
+  log: Math.log,
+
+  // Not sure if this is too inconvenient to use
+  '**': Math.pow,
+
+  //TODO logBase
+
+  sin: Math.sin,
+
+  tan: Math.tan,
+
+  cos: Math.cos,
+
+  asin: Math.asin,
+
+  atan: Math.atan,
+
+  acos: Math.acos,
+  //TODO: h variation
+
+  truncate: function(x) {
+    return x >= 0 ? _.floor(x) : x;
+  },
+
+  round: Math.round,
+
+  ceiling: Math.ceil,
+
+  floor: Math.floor,
+
+  floatRadix: function(x) {
+    return 2;
+  },
+
+  isInfinite: function(x) {
+    return x === Infinity;
+  },
+
+  id: _.identity,
+
+  'const': function(x, y) {
+    return x;
+  },
+
+  '.': _.compose,
+
+  '++': function(xs, ys) {
+    return xs.concat(ys);
+  },
+
+  init: function(xs) {
+    return arr.slice(0, -1);
+  },
+
+  'null': function(xs) {
+    return xs.length === 0;
+  },
+
+  '!!': function(idx, xs) {
+    return xs[idx];
+  },
+
+  // _'s foldl1 works when no starting value given
+  foldl1: _.foldl,
+
+  and: function(xs) {
+    return _.all(xs, _.id);
+  },
+
+  or: function(xs) {
+    return _.any(xs, _.id);
+  },
+
+  sum: function(xs) {
+    return _.foldl(xs, _['+'], 0);
+  },
+
+  product: function(xs) {
+    return _.foldl(xs, _['*'], 1);
+  },
+
+  concat: function(xss) {
+    return _.foldl(xss, _['++'], []);
+  },
+
+  //TODO: compose?
+  //TODO: figure out argument order. this is getting silly
+  concatMap: function(fn, xs) {
+    return _.concat(_.map(xs, fn));
   }
+
+  maximum: _.max,
+
+  minimum: _.min,
+  //TODO: length is reserved
+
+  //TODO: float stuff
+
+  //TODO: succ? could i use byte/charAt?
 });
